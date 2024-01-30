64b0d178575c57f2e717f43049f02804a2c80004
Joseph Woodward <joseph.woodward@xeuse.com>
2024-01-30 23:27:54 +0000

    feat: inital server working

--- src/server.rs
+++ src/server.rs
@@ -1,12 +1,36 @@
-use std::net::TcpListener;
+use std::{
+    io::{BufRead, BufReader},
+    net::{TcpListener, TcpStream},
+};
 
 pub struct Server {
     addr: String,
-    conn: Option<TcpListener>,
 }
 
 impl Server {
     pub fn new(addr: String) -> Self {
-        Self { addr, conn: None }
+        Self { addr }
+    }
+
+    pub fn run(&mut self) {
+        let listener = TcpListener::bind(&self.addr).unwrap();
+
+        for stream in listener.incoming() {
+            let stream = stream.unwrap();
+            self.handle_connection(stream);
+
+            println!("Connection established!");
+        }
+    }
+
+    fn handle_connection(&mut self, mut stream: TcpStream) {
+        let buf_reader = BufReader::new(&mut stream);
+        let http_request: Vec<_> = buf_reader
+            .lines()
+            .map(|result| result.unwrap())
+            .take_while(|line| !line.is_empty())
+            .collect();
+
+        println!("Request: {:#?}", http_request);
     }
 }
