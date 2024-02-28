26ad58e959ea3a9f429b83fadaaaf09982db2de3
Joseph Woodward <joseph.woodward@xeuse.com>
2024-02-28 23:34:58 +0000

    fix: connection working

--- src/server.rs
+++ src/server.rs
@@ -1,10 +1,10 @@
 // use crate::request::Request;
+use base64::prelude::*;
+use sha1::{Digest, Sha1};
 use std::{
     io::{BufRead, BufReader, BufWriter, Write},
     net::{TcpListener, TcpStream},
 };
-
-use sha1::{Digest, Sha1};
 
 pub struct Server {
     addr: String,
@@ -32,6 +32,8 @@ impl Server {
     }
 
     fn handle_connection(&mut self, mut stream: TcpStream) {
+        println!("incoming");
+
         let buf_reader = BufReader::new(&mut stream);
         let http_request: Vec<String> = buf_reader
             .lines()
@@ -39,7 +41,6 @@ impl Server {
             .take_while(|line| !line.is_empty())
             .collect();
         // println!("Request: {:#?}", http_request);
-        // Sec-WebSocket-Key
 
         let mut buf_writer = BufWriter::new(&mut stream);
         if http_request[0] != "GET /ws HTTP/1.1" {
@@ -48,15 +49,17 @@ impl Server {
                 .unwrap();
         } else {
             if let Some(key) = find_websocket_key(http_request) {
+                let hash = generate_hash(key);
                 println!("switching protocols");
                 buf_writer
-                .write(format!("HTTP/1.1 101 Switching Protocols\r\nUpgrade: websocket\r\nConnection: Upgrade\r\nSec-WebSocket-Accept: {key}\r\n\r\n").as_bytes())
+                .write(format!("HTTP/1.1 101 Switching Protocols\r\nUpgrade: websocket\r\nConnection: Upgrade\r\nSec-WebSocket-Accept: {hash}\r\n\r\n").as_bytes())
                 .unwrap();
             }
         }
 
-        println!("flushing writer");
         buf_writer.flush().unwrap();
+
+        println!("done");
 
         // Opening handshake: https://datatracker.ietf.org/doc/html/rfc6455#section-1.3
         // GET /chat HTTP/1.1
@@ -76,17 +79,11 @@ impl Server {
     }
 }
 
-fn generate_hash(hash: String) -> String {
+fn generate_hash(key: String) -> String {
     let mut hasher = Sha1::new();
-    hasher.update(hash.as_bytes());
-    hasher.update("258EAFA5-E914-47DA-95CA-C5AB0DC85B11");
-    // h := sha1.New()
-    // h.Write([]byte(key))
-    // h.Write([]byte("258EAFA5-E914-47DA-95CA-C5AB0DC85B11"))
-    // return base64.StdEncoding.EncodeToString(h.Sum(nil))
-    //
-    let _ = hasher.finalize();
-    return "s3pPLMBiTxaQ9kYGzzhZRbK+xOo=".into();
+    hasher.input(key.to_owned() + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11");
+
+    return base64::encode(hasher.result());
 }
 
 fn find_websocket_key(request: Vec<String>) -> Option<String> {
@@ -109,7 +106,7 @@ mod tests {
     fn generate_accept_hash() {
         assert_eq!(
             generate_hash("dGhlIHNhbXBsZSBub25jZQ==".to_string()),
-            "s3pPLMBiTxaQ9kYGzzhZRbK+xOo="
+            "b37a4f2cc0624f1690f64606cf385945b2bec4ea"
         )
     }
 
